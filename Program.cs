789d8da92d40c0c250f94734bd4c7e740b00149e
reggiemclean <reginaldmclean@trentu.ca>
2017-11-30 01:25:12 -0800

    added consts

--- Program.cs
+++ Program.cs
@@ -6,23 +6,28 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Assignment1 {
-	
-	class population{
-		public int customerNum {get; set;}
-        public int demand {get; set;}
-	}
 
-	class program {
+  
+    class Program {
 
-		static void Main(string[] args)
+        const int GA_POPSIZE = 100;
+        const int GA_MAXITERATIONS = 1000;
+        const double GA_ELITISMRATE = 0.1;
+        const double GA_MUTATIONRATE = 0.2;
+        const double GA_CROSSOVERRATE = 0.9;
+
+        public int GlobalBestFitness = 999999999; // the global variable to keep track of the global best distance that has been found
+        static void Main(string[] args)
 		{
-			population[,] ga_population;
-			ga_population = initPop(100);
-			int changeForGit = 0;
-			for (int gen = 1 ; gen < 100; gen++)
+
+            List<ReadInFile();
+            List<int> ga_population = new List<int>();
+            ga_population = InitPop(100);
+			for (int gen = 1 ; gen <= 100; gen++)
 			{
-				// apply genetic operators
-				// do crossover
+                // apply genetic operators
+                // do crossover
+                OnePointCrossover(ga_population, 4);
 				// do mutation
 				// evaluate fitness of population individuals
 				// select new populations using some strategy
@@ -30,21 +35,44 @@ namespace Assignment1 {
 			}
 		}
 
-		static population[,] initPop(int pop_size)
+		static List<int> InitPop(int pop_size)
 		{
-			population[,] init_population = new population[pop_size,101];
+			List<int> init_population = new List<int>();
 			// do something to initialize the population
 			return init_population;
 		}
 
-		static population[] crossover()
+        //One point crossover
+        static void OnePointCrossover(List<int> Parents, int IndexForCross)
 		{
-			population[] someChromosome = new population[101];
-			// do some crossover
 
-			// can use code from coding theory assignment to find random string
-			return someChromosome;
+            //*******************************************************************
+            List<int> NewChromosome = new List<int>(); // list that will contain the results of the crossover
+            List<int> TempChromosome = new List<int>(); // temporary list to contain original values of one parent for swap
+
+
+
+
+
+
+            //*******************************************************************
+
 		}
+        //End of one point crossover
 
+        static int CalculateFitness(List<int> pop)
+        {
+            int fitness = 0;
+            foreach (int x in pop)
+            {
+                fitness = fitness + x;
+            }
+            return fitness;
+        }
+        static void ReadInFile()
+        {
+
+
+        }
 	}
 }
